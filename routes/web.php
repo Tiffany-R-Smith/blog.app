7fd121d9fa7dee92dd6145eeade4011bd7e9c21f
Tiffany Wilson <tiffany.r.smi@gmail.com>
2023-11-20 15:55:34 -0500

    Update the view the use a blade component. Established database connection.

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('posts, [
+        'posts' => Post::all()
+    ]);
+});
+
+Route::get('posts/{post}', function ($id) {
+    return view('post', [
+        'post' => Post::find($id)
+    ]);
 });
