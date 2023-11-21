37c5262d6992f960ff6b8ddd3fee9341b68641ab
Tiffany Wilson <tiffany.r.smi@gmail.com>
2023-11-20 19:43:22 -0500

    Created an eloquent model called Post.

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Models\Post;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -14,12 +15,12 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('posts, [
+    return view('posts', [
         'posts' => Post::all()
     ]);
 });
 
-Route::get('posts/{post}', function ($id) {
+Route::get("posts/{post}", function ($id) {
     return view('post', [
         'post' => Post::find($id)
     ]);
